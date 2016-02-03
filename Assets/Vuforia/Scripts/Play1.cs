df0dbf8c618471ffd06e01ceee4df68c0ac72209
Srinivas <sairao1996@gmail.com>
2016-02-03 06:12:37 +0530

    Added Video, Modified Scripts, Added .gitignore

--- Assets/Vuforia/Scripts/Play1.cs
+++ Assets/Vuforia/Scripts/Play1.cs
@@ -2,16 +2,30 @@
 using System.Collections;
 
 public class Play1 : MonoBehaviour {
-
+	
 	public MovieTexture movTexture;
+	public static int flag = 0;
 	// Use this for initialization
 	void Start () {
-		GetComponent<Renderer>().material.mainTexture = movTexture;
-		movTexture.Play();
+		// The commented script starts playing the video as soon as the app starts, but a more useful behaviour would be playing it when marker is detected. 
+		//		GetComponent<Renderer>().material.mainTexture = movTexture;
+		//		movTexture.Play();
+		//		GetComponent<AudioSource> ().Play ();
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-
+		if (flag == 1) {
+			GetComponent<Renderer>().material.mainTexture = movTexture;
+			movTexture.Play();
+			GetComponent<AudioSource> ().Play ();
+			flag = 0;
+		}
+		if (flag == 2) {
+			movTexture.Pause();
+			GetComponent<AudioSource>().Pause();
+			flag = 0;
+		}
+		
 	}
 }
